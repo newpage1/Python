d572bca76b8c6310bb89f513dcca145438c7ca59
maorui <saberui2010@163.com>
2015-01-24 21:07:28 +0800

    xml

---
+++ xml/student_info.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+
+class sc:
+	def __init__(self,sname,sid,sgrade,cid,cname,teacher,cscore,idx):
+		self.__sname = sname
+		self.__sid = sid
+		self.__sgrade = sgrade
+		self.__cid = cid
+		self.__cname = cname
+		self.__teacher = teacher
+		self.__cscore = cscore
+	        self.__idx = idx
+
+	def print_info(self):
+		print " NO.%s record in XML"%self.__idx
+		print " <sname> : %s "%self.__sname
+		print " <sid> : %s "%self.__sid
+		print " <sgrade> : %s "%self.__sgrade
+		print " <course> "
+		print " <cid> : %s "%self.__cid
+		print " <cname> : %s  "%self.__cname
+		print " <teacher> : %s  "%self.__teacher
+		print " <score> : %s"%self.__cscore
---
+++ xml/xml.py
@@ -0,0 +1,50 @@
+#coding=utf-8
+
+'''
+input: xml with particular DTD
+output: the data_set in the xml
+'''
+
+import re
+from student_info import sc
+
+def get_data(file_path):
+    sc_list = []
+    avl = open(file_path, 'rb')
+    str_tmp = avl.read()
+    avl.close()
+    #print str_tmp
+    scourse = re.findall(r'<!ELEMENT (\w+)\([^#].+\)>',str_tmp)
+    remine = list()
+    result = list()
+    for s in scourse:
+        tmp = r'<%s>(.+?)</%s>'%(s,s)
+        remine.append(re.compile(tmp,re.DOTALL))
+        
+    scour = re.findall(r'<!ELEMENT (\w+)\(#\w+\)>',str_tmp)
+    for si in scour:
+        s_temp = r'<%s>(.+?)</%s>'%(si,si)
+        result.append(re.compile(s_temp,re.DOTALL))
+
+    scourse_list = remine[0].findall(str_tmp)
+
+    for idx,item in enumerate(scourse_list):
+        s_name = result[0].findall(item)
+        s_id = result[1].findall(item)
+        s_grade = result[2].findall(item)
+        s_cid = result[3].findall(item)
+        s_cname = result[4].findall(item)
+        s_teacher = result[5].findall(item)
+        s_cscore = result[6].findall(item)
+            
+        sc_list.append(sc(s_name,s_id,s_grade,s_cid,s_cname,s_teacher,s_cscore,idx+1))
+    return sc_list
+    
+if __name__ == "__main__":
+    scourse_list = get_data('sc_info.xml')
+    
+    for sc in scourse_list:
+        sc.print_info()
+    
+
+   
