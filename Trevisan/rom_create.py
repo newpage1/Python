25c04a48ee95db2d90a523d547d0a15a07a68384
maorui <saberui2010@163.com>
2015-01-28 15:00:53 +0800

    no message

--- Trevisan/rom_create.py
+++ Trevisan/rom_create.py
@@ -4,7 +4,7 @@ input_text = str()
 result = str()
 input_text = 'MEMORY_INITIALIZATION_RADIX=2;\r\nMEMORY_INITIALIZATION_VECTOR=\r\n'
 input_text = input_text.lower()
-for i in range(224):
+for i in range(128):
     for j in range(32):
         result = result + choice(['0','1'])
     input_text = input_text + result + ',\r\n'
