a25e12bc22e58d6da3db29d61e8d2d214c8f089e
maorui <saberui2010@163.com>
2015-01-29 22:32:24 +0800

    no message

--- Trevisan/mif_create.py
+++ Trevisan/mif_create.py
@@ -1,14 +1,14 @@
 __author__ = 'Owner'
 input_text = str()
 from random import *
-input_text += 'WIDTH=16;\r\nDEPTH=256;\r\nADDRESS_RADIX=HEX;\r\nDATA_RADIX=BIN;\r\nCONTENT BEGIN\r\n'
+input_text += 'WIDTH=1;\r\nDEPTH=4096;\r\nADDRESS_RADIX=HEX;\r\nDATA_RADIX=BIN;\r\nCONTENT BEGIN\r\n'
 line_text = str()
-for i in range(256):
+for i in range(4096):
     address = hex(i)
     address = address[2:]
     address += ':'
     line_text += address
-    for j in range(16):
+    for j in range(1):
         ran_text = str(randrange(2))
         line_text += ran_text
     line_text += ';\r\n'
