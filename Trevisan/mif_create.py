d28fe6482ced450e07d6ce35eab23dd208b015d1
maorui <saberui2010@163.com>
2015-01-30 21:14:23 +0800

    no message

--- Trevisan/mif_create.py
+++ Trevisan/mif_create.py
@@ -1,14 +1,14 @@
 __author__ = 'Owner'
 input_text = str()
 from random import *
-input_text += 'WIDTH=1;\r\nDEPTH=4096;\r\nADDRESS_RADIX=HEX;\r\nDATA_RADIX=BIN;\r\nCONTENT BEGIN\r\n'
+input_text += 'WIDTH=12;\r\nDEPTH=512;\r\nADDRESS_RADIX=HEX;\r\nDATA_RADIX=BIN;\r\nCONTENT BEGIN\r\n'
 line_text = str()
-for i in range(4096):
+for i in range(512):
     address = hex(i)
     address = address[2:]
     address += ':'
     line_text += address
-    for j in range(1):
+    for j in range(12):
         ran_text = str(randrange(2))
         line_text += ran_text
     line_text += ';\r\n'
@@ -17,7 +17,7 @@ for i in range(4096):
 
 input_text += 'END;'
 
-output = open('bit_ram_init.mif','wb')
+output = open('init_fifo.mif','wb')
 output.write(input_text)
 output.flush()
 output.close()
